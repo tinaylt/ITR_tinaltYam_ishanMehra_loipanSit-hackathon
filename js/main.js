3068cdfed7e34787c85300c8825e7d22c2d7eae4
tinaylt <144871562+tinaylt@users.noreply.github.com>
2024-09-26 19:00:55 -0400

    adjusted all the broken code and styling the header
    This is to update all the broken code (including html, css and javascript). adding the template hero background, styling the hero background texts.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,13 @@
-//menu animation 
 
 const hamburger = document.querySelector('.hamburger');
 const mobileNav = document.querySelector('.mobile-nav');
-const lines = document.querySelectorAll('.line'); // 
+const lines = document.querySelectorAll('.line');
 
 function toggleMenu() {
-  hamburger.classList.toggle('active')
-  openMenu()
-  burgerCol()
-};
-
-function openMenu(){
-  mobileNav.classList.toggle('translated');
-}
-
-function burgerCol(){
-  lines.forEach(line => line.classList.toggle('active')); 
+    const openMenu = hamburger.classList.toggle('active');
+    mobileNav.classList.toggle('hide', !openMenu);
+    lines.forEach(line => line.classList.toggle('active', openMenu));
 }
 
 hamburger.addEventListener('click', toggleMenu);
-
-
-
-
-
 
